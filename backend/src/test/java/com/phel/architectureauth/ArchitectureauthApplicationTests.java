72bbeca4f1884a9a4db783103b68d29eb5909529
Phel1405 <juanfeliperodriguez11c@gmail.com>
2026-08-12 12:37:20 -0500

    Estructura inicial de springboot y app de android

---
+++ backend/src/test/java/com/phel/architectureauth/ArchitectureauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phel.architectureauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
