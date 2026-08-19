95cd086dc6913a465b4864beeaf016ced7f68024
Phel1405 <juanfeliperodriguez11c@gmail.com>
2026-08-19 13:26:36 -0500

    Backend: Config basica de security

---
+++ backend/src/main/kotlin/com/phel/architectureauth/controller/AuthController.kt
@@ -0,0 +1,4 @@
+package com.phel.architectureauth.controller
+
+class AuthController {
+}
---
+++ backend/src/main/kotlin/com/phel/architectureauth/security/SecurityConfig.kt
@@ -0,0 +1,19 @@
+package com.phel.architectureauth.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    fun securityFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
+        return httpSecurity
+            .csrf { csrf -> csrf.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .build()
+    }
+}
